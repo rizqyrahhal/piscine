9118558057f3f379911f8067066460466a996308
rizqyrahhal <rizqyrahhal8@gmail.com>
2021-11-25 18:40:38 +0100

    updat addition rush

---
+++ rush/rush-02.c
@@ -0,0 +1,12 @@
+#include "rush-02.h"
+
+int     main(int argc, char **argv)
+{
+    (void)argc;
+    (void)argv;
+
+
+
+    write(1, "from main\n", ft_strlen("from main\n"));
+    return (0);
+}
---
+++ rush/rush-02.h
@@ -0,0 +1,8 @@
+#ifndef RUSH_02_H
+#define RUSH_02_H
+
+#include <unistd.h>
+
+int     ft_strlen(char *str);
+
+#endif
